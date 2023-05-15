dd3afde32c83a3ccf3910c1e71e43300c6146c47
Jason X <59701038+JChunX@users.noreply.github.com>
2023-05-14 22:41:29 -0700

    procfile

--- app.py
+++ app.py
@@ -113,4 +113,5 @@ def process_query():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
